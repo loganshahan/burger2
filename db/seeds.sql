89694fcd8eeaf3144292b51f3c21dc1d17630a91
Logan Shahan <loganshahan88@gmail.com>
2019-03-24 14:49:04 -0400

    working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Bacanator');
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name) VALUES ('Turkey');
